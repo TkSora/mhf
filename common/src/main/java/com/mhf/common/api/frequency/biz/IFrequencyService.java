cef7b359cdbca14b53f14a8ff941136e25dc88fe
bbjohn <295322187@qq.com>
2017-11-27 14:33:51 +0800

    添加模块 1、freemarker 2、frequency 频率限制 3、redis

---
+++ common/src/main/java/com/mhf/common/api/frequency/biz/IFrequencyService.java
@@ -0,0 +1,29 @@
+package com.mhf.common.api.frequency.biz;
+
+
+
+/*****************************************************************************/
+
+import com.mhf.common.api.frequency.domain.FrequencyDetail;
+
+/**
+ * Project Name:onlineleasing<br/>
+ * Package Name:com.sbm.module.onlineleasing.shop.biz<br/>
+ * File Name:ITOLShopService.java<br/>
+ * 
+ * 作成日 ：2017-5-18 下午4:34:06 <br/>
+ * 
+ * @author ：junkai.zhang
+ */
+public interface IFrequencyService {
+
+	/**
+	 * 
+	 * checkFrequencyDetail:检查频率
+	 * 
+	 * @author junkai.zhang
+	 * @param obj
+	 */
+	public void checkFrequencyDetail(FrequencyDetail obj);
+
+}
